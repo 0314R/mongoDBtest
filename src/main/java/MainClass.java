e4b9497e60774635e7d71380e422aefa96b37921
ramo0301 <o.adham@student.rug.nl>
2020-03-14 16:26:14 +0100

    cleaned the MainClass for new assignment

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -20,22 +20,6 @@ public class MainClass {
             System.out.println("THREW EXCEPTION UNKNOWN HOST");
         }
 
-        DB database = mongoClient.getDB("javaDB");
-        DBCollection collection = database.getCollection("employees");
-
-        Person personObj = new Person();
-        DBObject person1 = toDBObject(personObj);
-
-        DBObject person = new BasicDBObject("_id", "jo1")
-                                    .append("name", "Jo Bloggs")
-                                    .append("address", new BasicDBObject("street", "123 Fake Street")
-                                                                        .append("City", "Faketon")
-                                                                        .append( "state" , "MA")
-                                                                        .append("zip", 12345));
-        collection.insert(person);
-        collection.insert(person1);
-
-
         mongoClient.close();
     }
 }
